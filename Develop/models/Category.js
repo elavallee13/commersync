5eef2a9a3caf1e5fb25d56dbe46ff8a69eb6d48d
Eric L <126723001+elavallee13@users.noreply.github.com>
2023-08-16 15:38:20 -0500

    Added new code and made edits to starter code.

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -2,11 +2,39 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // ecommerce creator defines columns here
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+class Category extends Model {}
+
+Category.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
