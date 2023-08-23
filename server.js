cc4c8c158e6ce97ee9b75eb5a5e4a6f1128bf779
Eric L <126723001+elavallee13@users.noreply.github.com>
2023-08-23 18:40:17 -0500

    Added HTML and CSS

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
